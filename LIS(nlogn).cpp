98a2b47034dbbd6e1d842b8acf705ff5929b5dba
Saransh <114410043+saranshs17@users.noreply.github.com>
2024-01-22 15:41:06 +0530

    Create LIS(nlogn).cpp
    Note : temp.size() will give the size of longest increasing subsequence , it will not give the subsequence .

---
+++ LIS(nlogn).cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;cin>>n;
+    vector<int> v(n);
+    for(int i=0;i<n;++i)cin>>v[i];
+    vector<int> temp;
+    temp.push_back(v[0]);
+    for(int i=1;i<n;++i){
+        if(v[i]>temp.back()){
+            temp.push_back(v[i]);
+        }else{
+            int index=lower_bound(temp.begin(),temp.end(),v[i])-temp.begin();
+            temp[index]=v[i];
+        }
+    }
+    cout<<temp.size()<<'\n';
+}
