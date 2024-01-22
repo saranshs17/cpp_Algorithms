f117907aa5c8a6c1da9ebdde354a840f5c3bf7d0
Saransh <114410043+saranshs17@users.noreply.github.com>
2024-01-22 20:00:51 +0530

    Update LIS(nlogn).cpp
    This returns size of STRICTLY INCREASING INCREASING SUBSEQ
    
    FOR NON DECREASING SUBSEQUENCE 
    Eg - array = 1 , 2 , 3 , 2 , 3 , 5 , -> lis = ( 1 , 2 , 2 , 3 , 5 ) ,  ans = 5
    For this use upper_bound rather than lower_bound ,

--- LIS(nlogn).cpp
+++ LIS(nlogn).cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main(){
     int n;cin>>n;
-    vector<int> v(n);
+    vector<int> v(n); 
     for(int i=0;i<n;++i)cin>>v[i];
     vector<int> temp;
     temp.push_back(v[0]);
